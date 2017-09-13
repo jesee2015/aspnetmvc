ebf64b009f41fd4898e063011a9d0aff0791fe3b
柳曦 <sqtjug@126.com>
2017-09-13 13:57:53 +0800

    自定义mvc Route

---
+++ MvcApplication/RawContentResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MvcApplication
+{
+    class RawContentResult : ActionResult
+    {
+        private string content;
+
+        public RawContentResult(string content)
+        {
+            // TODO: Complete member initialization
+            this.content = content;
+        }
+    }
+}
---
+++ MvcApplication/Route.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Routing;
+
+namespace MvcApplication
+{
+    public class Routeddd : RouteBase
+    {
+        public IRouteHandler RouteHandler;
+        public string Url;
+
+        public override RouteData GetRouteData(HttpContextBase httpContext)
+        {
+            IDictionary<string, object> variables;
+            IDictionary<string, object> dataTokens;
+            if (this.Match(httpContext.Request.AppRelativeCurrentExecutionFilePath.Substring(2), out variables))
+            {
+                RouteData routeData = new RouteData();
+                foreach (var item in variables)
+                {
+                    routeData.Values.Add(item.Key, item.Value);
+                }
+                //foreach (var item in dataTokens)
+                //{
+                //    routeData.DataTokens.Add(item.Key, item.Value);
+                //}
+                routeData.RouteHandler = this.RouteHandler;
+                return routeData;
+            }
+            return null;
+        }
+
+        private bool Match(string requestUrl, out IDictionary<string, object> variables)
+        {
+            //variables = new Dictionary<string, object>();
+            //string strArray1 = requestUrl.Split('/');
+            variables = new Dictionary<string, object>();
+            return false;
+        }
+    }
+}
